c1581c586ec07dc796ccca1c0121b43445425527
Puansangapni <puansangapni@gmail.com>
2024-01-30 10:56:14 +0900

    Day 2/366 (Reversed Sequence)

---
+++ 2-reverseSeq.js
@@ -0,0 +1,29 @@
+// Coding Challenge 2024
+// Day 2/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+// ---------------------------- Cara 1 ----------------------------------
+
+// const reverseSeq = n => {
+//     const result = [];
+//     for (let i = n; i >= 1; i--){
+//         result.push(i);
+//     }
+//     return result;
+//   };
+
+// ---------------------------- Cara 2 ---------------------------------
+
+// const reverseSeq = (n) => {
+//     return Array(n).fill().map((el, i) => i + 1).reverse();
+// };
+
+// ---------------------------- Cara 3 ---------------------------------
+
+// const reverseSeq = (n) => [...Array(n)].fill().map((el, i) => i + 1).reverse();
+
+// ---------------------------- Cara 4 ---------------------------------
+
+const reverseSeq = (n) => [...Array(n)].fill().map((el, i) => n - i);
+
+console.log(reverseSeq(5))
